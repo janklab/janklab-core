a4148395e94dcecdcf473082e629f5a9cdb6e4b2
Andrew Janke <andrew@apjanke.net>
2018-02-01 04:04:05 -0500

    System: add sleep()

--- dist/Mcode/classes/+jl/+util/System.m
+++ dist/Mcode/classes/+jl/+util/System.m
@@ -15,6 +15,26 @@ classdef System
         out = str2double(procNumStr);
         end
         
+        function sleep(seconds, millis, nanos)
+        %SLEEP Make the Matlab execution thread sleep for a given time
+        %
+        % jl.util.System.sleep(seconds, millis, nanos)
+        %
+        % Seconds, millis, and nanos are all optional, and all accept fractional
+        % values. The resulting sleep time is rounded off to nanos.
+        %
+        % The actual sleep time is dependent on the precision and accuracy of
+        % system timers and schedulers. It's unlikely your precision is actually
+        % close to the nanosecond.
+        if nargin < 1 || isempty(seconds);  seconds = 0;  end
+        if nargin < 2 || isempty(millis);   millis = 0;   end
+        if nargin < 3 || isempty(nanos);    nanos = 0;    end
+        totalNanos = (seconds * 10^9) + (millis * 10^6) + nanos;
+        totalMillis = floor(totalNanos / 10^6);
+        additionalNanos = rem(totalMillis, 10^6);
+        java.lang.Thread.sleep(totalMillis, additionalNanos);
+        end
+        
     end
     
     methods (Access = private)
