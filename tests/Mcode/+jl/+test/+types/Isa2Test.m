996f7ac7e2bb438bc69e21257ecdf79f81e381ad
Andrew Janke <andrew@apjanke.net>
2018-02-01 04:56:27 -0500

    tests: add a basic isa2() test

---
+++ tests/Mcode/+jl/+test/+types/Isa2Test.m
@@ -0,0 +1,30 @@
+classdef Isa2Test < matlab.unittest.TestCase
+   
+     methods (Test)
+         function testSomeIsa2Cases(t)
+         % Test some example cases of isa2()
+         
+         dt = datetime;
+         
+         % { Value, Type, ExpectedAnswer; ... }
+         checks = {
+             42         'numeric'   true
+             42         'double'    true
+             42         'integer'   false
+             int32(42)  'integer'   true
+             {}         'cellstr'   true
+             {'foo','bar'}      'cellstr'   true
+             {'foo','bar',42}   'cellstr'   false
+             dt         'object'    true
+             dt         'datetime'  true
+             dt         'double'    false
+             dt         'numeric'   false
+             };
+         
+         for i = 1:size(checks, 1)
+             [value,type,expectedAnswer] = checks{i,:};
+             t.verifyEqual(isa2(value, type), expectedAnswer);
+         end
+         end
+     end
+end
