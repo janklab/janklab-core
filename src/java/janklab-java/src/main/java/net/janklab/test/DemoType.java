8bb001c93ddb37b529029ecfefddee4332a9f22a
Andrew Janke <andrew@apjanke.net>
2018-07-19 01:08:49 -0400

    Add String stuff to DemoType

--- src/java/janklab-java/src/main/java/net/janklab/test/DemoType.java
+++ src/java/janklab-java/src/main/java/net/janklab/test/DemoType.java
@@ -6,4 +6,15 @@ public class DemoType {
         return new java.lang.Integer(42);
     }
 
+    public Integer getDataAsInteger() {
+        return new java.lang.Integer(42);
+    }
+
+    public Object getStringAsObject() {
+        return "foo";
+    }
+
+    public String getStringAsString() {
+        return "foo";
+    }
 }
