1d6402e140efb4ef331c43368fe113cdb85502a8
Andrew Janke <andrew@apjanke.net>
2019-04-21 07:52:33 -0400

    Fix error handling in spew()

--- Mcode/classes/+jl/+io/spew.m
+++ Mcode/classes/+jl/+io/spew.m
@@ -5,6 +5,13 @@ function spew(file, data)
 if fid < 0
     error('jl:io', 'Failed opening file ''%s'': %s', file, msg);
 end
-RAII.fid = onCleanup(@() fclose(fid));
 
 fwrite(fid, data);
+
+status = fclose(fid);
+if status < 0
+    error('jl:io', 'Failed closing file after writing. Write may have failed. File: ''%s''', ...
+        file);
+end
+
+end
