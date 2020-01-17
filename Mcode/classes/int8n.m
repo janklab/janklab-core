6ceb782dad1baa80fc5f33baf957ae615d294f97
Andrew Janke <andrew@apjanke.net>
2020-01-16 21:50:37 -0500

    WIP: Start work on NaN-able ints

---
+++ Mcode/classes/int8n.m
@@ -0,0 +1,14 @@
+classdef int8n < intn
+	%INT8N NaN-able signed 8-bit integer array
+	
+	methods
+		function this = int8n(ints, tfnan)
+			if nargin < 2 || isempty(tfnan)
+				tfnan = isnan(ints);
+			end
+			ints = int8(ints);
+			this = this@intn(ints, tfnan);
+		end
+		
+	end
+end
