f5b8315e1b2eae9399ab9f3af873d28ae84da837
Andrew Janke <andrew@apjanke.net>
2020-01-16 22:41:36 -0500

    Add TODO

--- Mcode/classes/intn.m
+++ Mcode/classes/intn.m
@@ -27,7 +27,7 @@ classdef (Abstract) intn < jl.util.Displayable
 	% See also:
 	% INT8N, UINT8N, INT16N, UINT16N, INT32N, UINT32N, INT64N, UINT64N
 
-	% TODO: Decide whether this should respond true to isinteger(obj)
+	% TODO: Decide whether this should respond true to isinteger() and isnumeric()
 	% TODO: Max, min, mean, median, diff
 	% TODO: cumprod, cumsum
 	
