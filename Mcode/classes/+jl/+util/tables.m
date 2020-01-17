69a9d0ae7bce4f83a9cda714efa13f870c266b36
Andrew Janke <andrew@apjanke.net>
2020-01-17 04:00:38 -0500

    tables: Remove redundant suppliers-parts example

--- Mcode/classes/+jl/+util/tables.m
+++ Mcode/classes/+jl/+util/tables.m
@@ -103,56 +103,6 @@ classdef tables
             end
         end
         
-        function [S,P,SP] = supplierPartsExample()
-            %SUPPLIERPARTSEXAMPLE The classic "Supplier Parts" example database
-            %
-            % s = jl.util.tables.supplierPartsExample
-            % [S,P,SP] = jl.util.tables.supplierPartsExample
-            %
-            % Generates the classic C. J. Date "Supplier Parts" example database
-            % as a set of table objects.
-            %
-            % If a single output is captured, returns a struct with fields S, P,
-            % and SP. If multiple outputs are captured, returns S, P, and SP as
-            % separate outputs.
-            S = cell2table({
-                'S1'    'Smith'     20  'London'
-                'S2'    'Jones'     10  'Paris'
-                'S3'    'Blake'     30  'Paris'
-                'S4'    'Clark'     20  'London'
-                'S5'    'Adams'     30  'Athens'
-                }, 'VariableNames', {'SNum','SName','Status','City'});
-            P = cell2table({
-                'P1'    'Nut'   'Red'   12.0    'London'
-                'P2'    'Bolt'  'Green' 17.0    'Paris'
-                'P3'    'Screw' 'Blue'  17.0    'Oslo'
-                'P4'    'Screw' 'Red'   14.0    'London'
-                'P5'    'Cam'   'Blue'  12.0    'Paris'
-                'P6'    'Cog'   'Red'   19.0    'London'
-                }, 'VariableNames', {'PNum','PName','Color','Weight','City'});
-            SP = cell2table({
-                'S1'    'P1'    300
-                'S1'    'P2'    200
-                'S1'    'P3'    400
-                'S1'    'P5'    100
-                'S1'    'P6'    100
-                'S2'    'P1'    300
-                'S2'    'P2'    400
-                'S3'    'P2'    200
-                'S4'    'P2'    200
-                'S4'    'P4'    300
-                'S4'    'P5'    400
-                }, 'VariableNames', {'SNum','PNum','Qty'});
-            
-            if nargout == 1
-                tmp = S;
-                S = struct;
-                S.S = tmp;
-                S.P = P;
-                S.SP = SP;
-            end
-        end
-        
         function out = tableFromStructRecArray(s)
             %TABLEFROMSTRUCTRECARRAY Convert array of record structs to table
             %
