763b85081e713793f65542662deceab6400fe252
Andrew Janke <andrew@apjanke.net>
2020-01-17 05:05:41 -0500

    Add ellipses()

---
+++ Mcode/classes/+jl/+util/ellipses.m
@@ -0,0 +1,20 @@
+function out = ellipses(x, n)
+%ELLIPSES Convert array into a possibly-truncated list of values
+%
+% out = jl.util.ellipses(x, n)
+%
+% x may be anything that supports dispstrs(). (Which is basically anything.)
+%
+% n is the maximum number of elements to include in the output before truncating
+% the list.
+%
+% Returns a scalar string
+
+strs = dispstrs(x);
+strs = strs(:)';
+if numel(strs) > n
+  strs(n+1:end) = [];
+  strs(end+1) = {'...'};
+end
+out = strjoin(strs, ', ');
+end
