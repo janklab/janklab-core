72c5340f1730162b389fbde424304e179ec638e6
Andrew Janke <andrew@apjanke.net>
2020-01-18 16:48:27 -0500

    strlen: helptext tweak

--- Mcode/toplevel/strlen.m
+++ Mcode/toplevel/strlen.m
@@ -1,7 +1,11 @@
 function out = strlen(str)
 %STRLEN Length of strings in char, cellstr, or string array
 %
+% out = strlen(str)
+%
 % Counts the length of each string in the input, as a number of characters.
+%
+% str may be a string array, cellstr, or char array.
 %
 % Char arrays are considered to be column vectors of strings, and trailing blanks are
 % not counted.
