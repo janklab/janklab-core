b894582225abf7a7a9ccb1ffc9a160caa6421697
Andrew Janke <andrew@apjanke.net>
2021-09-20 14:47:16 -0500

    planargen: fix for internal string-ification

--- Changelog.md
+++
@@ -1,1 +0,0 @@
-doc/Changelog.md
---
+++ Mcode/classes/+jl/+code/+internal/PlanarClassBoilerplateParts.m
@@ -0,0 +1,10 @@
+classdef PlanarClassBoilerplateParts
+    
+    properties
+        publicDefns (:,1) string = []
+        protectedDefns (:,1) string = []
+        privateDefns (:,1) string = []
+        localFcnDefns (:,1) string = []
+    end
+    
+end
